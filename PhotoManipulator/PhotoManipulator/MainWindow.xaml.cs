0db4bcf7e5088b9d4fc4e46302f0ae8cf442977b
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-03-02 22:36:07 -0800

    Add files via upload

---
+++ PhotoManipulator/PhotoManipulator/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoManipulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog newImage = new OpenFileDialog();
+            newImage.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (newImage.ShowDialog() == true)
+            {
+                ImageViewer.Source = new BitmapImage(new Uri(newImage.FileName));
+            }
+        }
+    }
+}
